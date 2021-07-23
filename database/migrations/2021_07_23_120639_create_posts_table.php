f6081c120cd9813d60389830a0b9083aef826ec8
Edomak <edoardo.maccherini@gmail.com>
2021-07-23 14:18:57 +0200

    seeder

--- database/migrations/2021_07_23_120639_create_posts_table.php
+++ database/migrations/2021_07_23_120639_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
             $table->timestamps();
         });
     }
