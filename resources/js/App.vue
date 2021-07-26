b521f2c41c86447b5f82628b76b6c685ec20b3b1
Edomak <edoardo.maccherini@gmail.com>
2021-07-26 14:08:23 +0200

    istanza vue + show

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <WorkInProgress />
+</template>
+
+<script>
+import WorkInProgress from './components/WorkInProgress.vue'
+
+export default {
+    name: 'App',
+    components: {
+        WorkInProgress
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
